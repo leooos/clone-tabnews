d0974c8ec8d5d0a789a36489800420279694ff2f
leooos <leokagohara@gmail.com>
2024-02-09 20:41:10 +0000

    Adicionado arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
-
-    return <h1>Hagatha, eu amo você. Se você me ama, dá uma risadinha 😬</h1>
+  
+  return <h1>Hagatha, eu amo você. Se você me ama, dá uma risadinha 😬</h1>
 
 }
 
