223337f4cf424fd208dce612744c282abed9d8e1
leooos <leokagohara@gmail.com>
2024-02-13 02:56:01 +0000

    Add tests, this is a mid commit

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,31 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const inUseConnections = await database.query(
+    "SELECT * FROM pg_stat_activity;",
+  );
+
   response.status(200).json({
-    msg: "Ihaaaaa!",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+      },
+    },
+    max_connections: databaseMaxConnectionsValue,
+    in_use_connections: Object.entries(inUseConnections.rows[0]).length,
   });
 }
 
