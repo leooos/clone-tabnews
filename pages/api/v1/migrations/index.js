c9e6f67101f7493785fba419f3695bbe81131ec5
Leonardo Kagohara <leonardo.kagohara@starkbank.com>
2026-06-17 00:54:03 -0300

    refactor: use `next-connect` in `/migrations` and `/status`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,15 +1,33 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller";
 import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 import database from "infra/database.js";
 
-export default async function migrations(request, response) {
-  const allowedMethods = ["GET", "POST"];
-  if (!allowedMethods.includes(request.method)) {
-    return response.status(405).json({
-      error: `Method "${request.method}" not allowed`,
-    });
-  }
+const router = createRouter();
 
+router.get(getHandler);
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(request, response) {
+  const isDryRun = true;
+  const pendingMigrations = await runMigrations(isDryRun);
+  return response.status(200).json(pendingMigrations);
+}
+
+async function postHandler(request, response) {
+  const isDryRun = false;
+  const migratedMigrations = await runMigrations(isDryRun);
+
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
+  }
+  return response.status(200).json(migratedMigrations);
+}
+
+async function runMigrations(isDryRun) {
   let dbClient;
 
   try {
@@ -17,33 +35,25 @@ export default async function migrations(request, response) {
 
     const defaultMigrationOptions = {
       dbClient: dbClient,
-      dryRun: true,
+      dryRun: isDryRun,
       dir: resolve("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
     };
 
-    if (request.method === "GET") {
+    if (isDryRun) {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-      return response.status(200).json(pendingMigrations);
+      return pendingMigrations;
     }
 
-    if (request.method === "POST") {
+    if (!isDryRun) {
       const migratedMigrations = await migrationRunner({
         ...defaultMigrationOptions,
         dryRun: false,
       });
-
-      if (migratedMigrations.length > 0) {
-        return response.status(201).json(migratedMigrations);
-      }
-
-      return response.status(200).json(migratedMigrations);
+      return migratedMigrations;
     }
-  } catch (error) {
-    console.error(error);
-    throw error;
   } finally {
     await dbClient.end();
   }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,7 +4,7 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-describe("GET /api/v1/migrations", () => {
+describe("GET /api/v1/status", () => {
   describe("Anonymous user", () => {
     test("Retrieving current system status", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
@@ -15,11 +15,6 @@ describe("GET /api/v1/migrations", () => {
 
       const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
       expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-
-      // expect(responseBody.dependencies.database.version).toEqual("16.2");
-
-      // expect(responseBody.max_connections).toBe(112);
-
       expect(responseBody.in_use_connections).toBe(1);
     });
   });
