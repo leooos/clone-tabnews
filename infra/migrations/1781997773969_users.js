0778ce47e09bb28249a9f27f9f7431396a4b2c61
Leonardo Kagohara <leonardo.kagohara@starkbank.com>
2026-06-20 21:06:27 -0300

    feat: add migration to create `users` table

---
+++ infra/migrations/1781997773969_users.js
@@ -0,0 +1,42 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()")
+    },
+    
+    // for reference Github limits username to 39 chars
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+
+    //bcrypt output is always 60 chars long
+    password: {
+      type: "varchar(60)",
+      notNull: true,
+    },
+
+    createdAt: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('UTC', now())"),
+    },
+
+    updatedAt: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('UTC', now())"),
+    }
+  })
+};
+
+exports.down = false
